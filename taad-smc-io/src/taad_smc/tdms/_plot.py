b4c92099f5c9c8f944e90afc7de01d610d439587
Will Zhang <willwz@gmail.com>
2025-09-29 09:57:58 -0400

    update plot for segment

--- taad-smc-io/src/taad_smc/tdms/_plot.py
+++ taad-smc-io/src/taad_smc/tdms/_plot.py
@@ -11,8 +11,17 @@ from .struct import TDMSData
 
 
 def plot_data[F: np.floating](data: TDMSData[F], *, fout: Path) -> None:
-    fig, ax = create_figure(2, figsize=(16, 4), dpi=180)
-    update_figure_setting(fig, padleft=0.05)
+    fig, ax = create_figure(2, figsize=(8, 3), dpi=180)
+    update_figure_setting(fig, padleft=0.2, padbottom=0.15)
+    # indices = data.time > 600.0
+    # time = data.time[indices] - 600.0
+    # disp = data.disp[indices]
+    # force = data.force[indices]
+    # ax[0].plot(time, force, "k-", label="Force (mN)")
+    # ax[0].set_xlabel("Time (s)")
+    # ax[0].set_ylabel("Force (mN)")
+    # ax[1].plot(time, force, "k-", label="Force (mN)")
+    # ax[0].plot(time, disp, "k-", label="Strain")
     ax[0].plot(data.time, data.disp, "k-", label="Strain")
     ax[0].set_xlabel("Time (s)")
     ax[0].set_ylabel("Strain")
